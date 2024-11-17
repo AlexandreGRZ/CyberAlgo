6347ec01fad96a552f6c3eb82a2b491ffb09a088
mathysfrc <mathysf41@gmail.com>
2024-11-17 22:16:00 +0100

    Mise en place d'un algo en 3DES côté Client

--- CyberSecurity/CyberSecurity/Controllers/HomeController.cs
+++ CyberSecurity/CyberSecurity/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace CyberSecurity.Controllers
 {
@@ -17,10 +20,44 @@ namespace CyberSecurity.Controllers
         }
         
         [HttpPut("cryptWith3DESmodeEBC")]
-        public bool cryptWith3DESmodeEBC()
+        public IActionResult CryptWith3DESmodeEBC([FromBody] string encryptedMessage)
         {
-            //todo :  msg chiffré avec 3DES en mode EBC , les clés sont hardcodé
-            return true;
+            try
+            {
+                if (string.IsNullOrEmpty(encryptedMessage))
+                    return BadRequest("Le message est vide ou invalide.");
+
+                byte[] key = Encoding.UTF8.GetBytes("123456789012345678901234");
+                byte[] encryptedBytes = Convert.FromBase64String(encryptedMessage); 
+                
+                using (TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider())
+                {
+                    tdes.Key = key;
+                    tdes.Mode = CipherMode.ECB;
+                    tdes.Padding = PaddingMode.PKCS7;
+
+                    ICryptoTransform decryptor = tdes.CreateDecryptor();
+                    byte[] decryptedBytes = decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+
+                    string decryptedMessage = Encoding.UTF8.GetString(decryptedBytes);
+
+                    Console.WriteLine($"Message déchiffré : {decryptedMessage}");
+
+                    if (decryptedMessage.Equals("cyber", StringComparison.OrdinalIgnoreCase))
+                    {
+                        return Ok("true"); 
+                    }
+                    else
+                    {
+                        return Ok("false");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur serveur : {ex.Message}");
+                return StatusCode(500, "Erreur lors du chiffrement.");
+            }
         }
         [HttpPut("cryptWithAESmodeCBC")]
         public bool cryptWithAESmodeCBC()
