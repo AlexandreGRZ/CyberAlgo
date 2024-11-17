ada0ffd43322d5b54686d43725121890849e083f
CedGav19 <cedric.gavage@student.hepl.be>
2024-11-17 14:19:20 +0100

    migration vers 2 api web

---
+++ ClientCyberAlgo/ClientCyberAlgo/Service/ApiService.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Diagnostics;
+using System.Text;
+using Newtonsoft.Json;
+namespace ClientCyberAlgo.Service
+{
+    public class ApiService
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<string> PutDataAsync(string url, object data)
+        {
+            try
+            {
+                string jsonData = JsonConvert.SerializeObject(data);
+                HttpContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                Debug.WriteLine(jsonData);
+                HttpResponseMessage response = await _httpClient.PutAsync(url, content);
+                Debug.WriteLine(response);
+                response.EnsureSuccessStatusCode();
+
+                string responseData = await response.Content.ReadAsStringAsync();
+                Debug.WriteLine(responseData);
+                return responseData;
+            }
+            catch (HttpRequestException e)
+            {       
+                Console.WriteLine($"Erreur de requête HTTP: {e.Message}");
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur : {ex.Message}");
+                return null;
+            }
+        }
+       
+    }
+
+}
