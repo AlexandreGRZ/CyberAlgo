bfa2e6bbd324980d0e242f8dac27121eed967ab1
mathysfrc <mathysf41@gmail.com>
2024-11-21 17:57:04 +0100

    Mise en place propre de l'algo 3DES mode ECB

--- CyberSecurity/CyberSecurity/obj/Debug/net8.0/CyberSecurity.AssemblyInfo.cs
+++ CyberSecurity/CyberSecurity/obj/Debug/net8.0/CyberSecurity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberSecurity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27cf3392082df582c4faabf4d8f1c3147baea3a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6347ec01fad96a552f6c3eb82a2b491ffb09a088")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberSecurity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberSecurity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
